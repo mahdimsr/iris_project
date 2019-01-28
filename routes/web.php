d5e9c29873bc476d1edb40bec7711e0812a4fb5b
Amir Pirmoradian <amirpirmoradian94@gmail.com>
2019-01-29 01:30:08 +0330

    add meetings routes

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@
 */
 
 use Illuminate\Support\Facades\Route;
-
-
 Route::get('/', function()
 {
 	return view('welcome');
@@ -21,17 +19,19 @@ Route::get('/', function()
 
 Route::prefix('dashboard')->group(function()
 {
-	Route::get('/view', 'Dashboard\\DashboardController@dashboardView');
+	Route::get('/view', 'DashboardController@dashboardView');
 
 
 	//meting
 	Route::prefix('meeting')->group(function()
 	{
-		Route::get('/view', 'Dashboard\\MeetingController@View');
-		Route::get('/insertView', 'Dashboard\\MeetingController@insertView');
-		Route::post('/insert', 'Dashboard\\MeetingController@insert');
-		Route::get('/editView/{id}', 'Dashboard\\MeetingController@editView');
-		Route::post('/remove', 'Dashboard\\MeetingController@remove');
+        Route::get('/view', 'MeetingController@View')->name('meetings');
+        Route::get('/show/{Meeting}', 'MeetingController@Show')->name('meetings-show');
+		Route::get('/insertView', 'MeetingController@insertView');
+		Route::post('/insert', 'MeetingController@insert');
+        Route::get('/editView/{Meeting}', 'MeetingController@editView')->name('meetings-edit');
+        Route::post('/edit', 'MeetingController@edit')->name('meetings-edit-post');
+		Route::get('/remove/{Meeting}', 'MeetingController@remove')->name('meetings-remove');
 	});
 
 });
