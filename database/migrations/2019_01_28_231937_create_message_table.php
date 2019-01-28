6df071effab724de643b6a4b9c586bfd20bc657e
Amir Pirmoradian <amirpirmoradian94@gmail.com>
2019-01-29 03:13:43 +0330

    Create Message Model and migration

---
+++ database/migrations/2019_01_28_231937_create_message_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('sender_id');//if 0 it mean system
+            $table->text('receiver_id');//if 0 it mean all
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
