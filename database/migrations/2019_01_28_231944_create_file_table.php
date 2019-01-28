5908785fa79c86c0f2ea30e8a74353ddb93a2f4f
Amir Pirmoradian <amirpirmoradian94@gmail.com>
2019-01-29 03:25:02 +0330

    add type and is_approved column

--- database/migrations/2019_01_28_231944_create_file_table.php
+++ database/migrations/2019_01_28_231944_create_file_table.php
@@ -17,6 +17,8 @@ class CreateFileTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('path');
+            $table->enum('type',['FiNAL','DOC'])->nullable();
+            $table->boolean('is_approved')->default(false);
             $table->integer('meeting_id');
             $table->integer('uploader_id');//if 0 it mean system
             $table->timestamps();
