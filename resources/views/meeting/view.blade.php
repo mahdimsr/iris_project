d956f79a788e21f688b39644c16affa155e5098c
Amir Pirmoradian <amirpirmoradian94@gmail.com>
2019-01-29 01:27:37 +0330

    add status session to view

--- resources/views/meeting/view.blade.php
+++ resources/views/meeting/view.blade.php
@@ -1,16 +1,22 @@
 @extends('layout')
 
 @section('content')
-
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-12 ">
+				@if (\Session::has('success'))
+					<div class="alert alert-success">
+						<ul>
+							<li>{!! \Session::get('success') !!}</li>
+						</ul>
+					</div>
+				@endif
 				<div class="card ">
 					<div class="header">
 						<h4 class="title set-font">جلسات</h4>
 						<p class="category set-font" style="margin-bottom: 15px">لیست تمامی جلسات سازمان</p>
 						<a class="btn-sm btn-info btn-fill set-font"
-						   href="{{action('Dashboard\\MeetingController@insertView')}}">ثبت جلسه</a>
+						   href="{{action('MeetingController@insertView')}}">ثبت جلسه</a>
 					</div>
 					<div class="content">
 						@if(count($meetings) > 0)
@@ -31,10 +37,13 @@
 										<td class="set-font">{{\App\Lib\Enum::meetingState($meeting->state)}}</td>
 										<td class="set-font">
 											<div class="row">
-												<a href="{{action('Dashboard\\MeetingController@editView',['id' => $meeting->id])}}"
+												<a href="{{route('meetings-show' , $meeting->id)}}"
+												   class="btn-sm btn-fill btn-success set-font">نمایش</a>
+
+												<a href="{{route('meetings-edit' , $meeting->id)}}"
 												   class="btn-sm btn-fill btn-info set-font">ویرایش</a>
 
-												<a href="#"
+												<a href="{{route('meetings-remove' , $meeting->id)}}"
 												   class="btn-sm btn-fill btn-danger set-font">حذف</a>
 											</div>
 										</td>
@@ -46,7 +55,7 @@
 							<div class="inline-block align-middle ">
 								<h1 class="font-weight-normal lead set-font" id="desc">هیچ جلسه ای وجود ندارد!!</h1>
 								<a class="btn btn-info btn-fill set-font"
-								   href="{{action('Dashboard\\MeetingController@insertView')}}">ثبت جلسه</a>
+								   href="{{action('MeetingController@insertView')}}">ثبت جلسه</a>
 							</div>
 						@endif
 					</div>
