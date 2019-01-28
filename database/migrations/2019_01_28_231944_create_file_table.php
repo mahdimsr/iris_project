898c37103d00f63ea716bb9483e6f7b43915482d
Amir Pirmoradian <amirpirmoradian94@gmail.com>
2019-01-29 03:13:21 +0330

    Create File Table and migration

---
+++ database/migrations/2019_01_28_231944_create_file_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('path');
+            $table->integer('meeting_id');
+            $table->integer('uploader_id');//if 0 it mean system
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file');
+    }
+}
